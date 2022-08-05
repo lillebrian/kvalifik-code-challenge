5f9fe42f0b5207679683bf0a5ee9abde93913bbc
lillebrian <s195456@student.dtu.dk>
2022-08-05 17:23:21 +0200

    Working on the first part

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,9 +1,18 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Grid, GridItem } from '@chakra-ui/react'
 import type { FC } from 'react'
+import { format, addDays } from 'date-fns'
 
 type Props = {
   param?: string
 }
+
+const Testdata = [
+  {date: new Date(2022, 1, 14)},
+  {date: new Date(2022, 1, 15)},
+  {date: new Date(2022, 1, 16)},
+  {date: new Date(2022, 1, 17)},
+
+]
 
 const Calendar: FC<Props> = () => {
   return (
@@ -15,10 +24,24 @@ const Calendar: FC<Props> = () => {
       boxShadow='0 0 99px 0px rgba(0, 0, 0, 0.04)'
       mb='10'
       display='flex'
-      justifyContent='center'
-      alignItems='center'
+      overflow='hidden'
+      //justifyContent='center'
+      //alignItems='center'
     >
-      <p>Calendar</p>
+      <Box display='flex' h='10%' w='100%' bg='teal.400' color='white'>
+        Calendar
+      </Box>
+      <Box display='flex'  bg='teal.400' color='white'>
+        <Grid display='flex'>
+          <GridItem></GridItem>
+          <GridItem> test 2</GridItem>
+          <GridItem> test 3</GridItem>
+          <GridItem> test 4</GridItem>
+          
+          {/* {Testdata.map(date => {return (<GridItem bg='gray.100'> {date.date} </GridItem>)})} */}
+            
+        </Grid>
+      </Box>
     </Box>
   )
 }
