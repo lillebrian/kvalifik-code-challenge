5cd1d3c4817aace0caf96a583ec4dd3e94811ac1
brian <s195456@student.dtu.dk>
2022-08-07 20:45:35 +0200

    Work on Calendar

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,6 +1,7 @@
-import { Box, Grid, GridItem } from '@chakra-ui/react'
-import type { FC } from 'react'
-import { format, addDays } from 'date-fns'
+import { Box, Grid, GridItem, SimpleGrid } from '@chakra-ui/react'
+import { FC, useEffect, useState } from 'react'
+import { format, addDays, endOfMonth, parse, startOfMonth } from 'date-fns'
+import { count } from 'console'
 
 type Props = {
   param?: string
@@ -11,10 +12,73 @@ const Testdata = [
   {date: new Date(2022, 1, 15)},
   {date: new Date(2022, 1, 16)},
   {date: new Date(2022, 1, 17)},
-
+  {date: new Date(2022, 1, 18)},
+  {date: new Date(2022, 1, 19)},
+  {date: new Date(2022, 1, 20)},
+  {date: new Date(2022, 1, 21)},
+  {date: new Date(2022, 1, 22)},
+  
 ]
 
+interface dates {
+  startOfMonth: Date
+  endOfMonth: Date
+  currentDay: Date
+}
+
+const allMonths = [
+  "jan",
+  "feb",
+  "mar",
+  "apr",
+  "maj",
+  "jun",
+  "jul",
+  "aug",
+  "sep",
+   "okt",
+   "nov",
+   "dec"
+]
+
+const renderDates = (datesToRender: dates) => {
+  const month: Date[] = []
+  const m: number = datesToRender.currentDay.getMonth()
+  
+  console.log("tester renderdates: \n" + m)
+  console.log("printer fundet måned: " + allMonths[m])
+  
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+  }
+  return("month")
+}
+
 const Calendar: FC<Props> = () => {
+  
+  const [currentDay, setCurrentDay] = useState<Date>(new Date());
+  const [monthStartDay, setMonthStartDay] = useState<Date>(startOfMonth(currentDay));
+  const [monthEndDay, setMonthEndDay] = useState<Date>(endOfMonth(currentDay));
+  
+  
+  
+  useEffect(() => {
+    // setCurrentDay(new Date())
+    // setMonthStartDay(startOfMonth(new Date()))
+    // setMonthEndDay(endOfMonth(new Date()))
+    const monthToRender: dates = {
+      startOfMonth: monthStartDay,
+      endOfMonth: monthEndDay,
+      currentDay: currentDay
+    }
+    renderDates(monthToRender)
+    console.log(currentDay)
+    console.log(monthStartDay)
+    console.log(monthEndDay)
+  }, [])
+  
+  
+  
   return (
     <Box 
       w='500px'
@@ -23,24 +87,27 @@ const Calendar: FC<Props> = () => {
       borderRadius='13px'
       boxShadow='0 0 99px 0px rgba(0, 0, 0, 0.04)'
       mb='10'
-      display='flex'
-      overflow='hidden'
-      //justifyContent='center'
-      //alignItems='center'
+      display=''
+      
+      // justifyContent='center'
+      alignItems='center'
     >
-      <Box display='flex' h='10%' w='100%' bg='teal.400' color='white'>
+      <Box textAlign='center' fontSize='2xl'  h='10%' w='100%' bg='teal.400' color='white'>
         Calendar
       </Box>
-      <Box display='flex'  bg='teal.400' color='white'>
-        <Grid display='flex'>
-          <GridItem></GridItem>
-          <GridItem> test 2</GridItem>
-          <GridItem> test 3</GridItem>
-          <GridItem> test 4</GridItem>
-          
-          {/* {Testdata.map(date => {return (<GridItem bg='gray.100'> {date.date} </GridItem>)})} */}
+      <Box h='90%' w='100%' bg='teal.100' color='white' overflow='hidden'>
+        <SimpleGrid columns={7}>
+          {Testdata.map(({date}, i) => {
+            return (<Box w='100%' h='100%' bg='white' border='1px' borderColor='black' textColor='black' key={i}> {format(date,"dd/mm/yyyy")}
+              </Box>);})}
+
+        </SimpleGrid>
+        {/* <Grid gridColumn='repeat(7, 1fr)' gridRow='repeat(5, 1fr)' >
+         
+          {console.log(Testdata[0].date)}
+          {Testdata.map(({date}, i) => {return (<GridItem bg='gray.100' textColor='black' key={i}> HEJSA </GridItem>);})}
             
-        </Grid>
+        </Grid> */}
       </Box>
     </Box>
   )
